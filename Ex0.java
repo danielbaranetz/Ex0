f2ca73938978315dead3fa680e9311a59388d127
danielbaranetz <dbaranetz@gmail.com>
2024-11-15 15:33:50 +0200

    added code requirements in my word

--- Ex0.java
+++ Ex0.java
@@ -1,3 +1,21 @@
+/*
+*****************************************************
+how the code should be in my own words step by step:
+1. ask the user to insert a number that even and greater than 4
+2.if the number is greater than 4 and even start count the prime numbers. else ask again to insert a valid number
+3.create function with 1 parameter that check if the number is prime
+4. call the function in every section to get only the prime numbers
+5. for section (a) take the number and subtract number with i and check in prime function if its prime and print it x + y = number
+6. for section (b) take the number and add number with i and check in prime function if its prime and print it as x - y = number
+7. for section (c) create list and store every prime number into the list and then print list.size and show the first in list and last in list
+8. for section (d) take number and check if divisible with i without remainder, store each factor in a list, and print the list with the factors multiplied together.
+9. for section (e) set in the start of the function  system.nanotime till the end of the function and do the same and calculate delta (end - start) and divide by 1_000_000_000.0 to get seconds
+10. for section (f) print my id number
+*****************************************************
+
+
+ */
+
 import java.util.*;
 import java.util.Scanner;
 
@@ -32,7 +50,7 @@ public class Ex0 {
                             myList.add(i);
                         }
                     }
-                    System.out.printf("c) There are %d prime numbers in %s%n", myList.size(), "[" + myList.getFirst() + "," + myList.getLast() + "]");
+                    System.out.printf("c) There are %d prime numbers in %s%n", myList.size(), "[" + myList.getFirst() + "," + number + "]");
 
                     List<Integer> primeNumbers = new ArrayList<>();
                     for ( int i = 2; i <=  number; i++) {
@@ -44,7 +62,7 @@ public class Ex0 {
                     StringBuilder result = new StringBuilder();
                     int product = 1;
                     for (int i = 0; i < primeNumbers.size(); i++) {
-                        result.append(primeNumbers.get(i)); // Append the factor
+                        result.append(primeNumbers.get(i));
                         product *= primeNumbers.get(i);
                         if (i < primeNumbers.size() - 1) {
                             result.append(" * ");
